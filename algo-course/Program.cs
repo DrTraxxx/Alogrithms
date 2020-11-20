f8662af2f4c84d02a908b098831049d2446a0551
traxxx <drtraxxxx@gmail.com>
2020-11-20 18:13:00 +0200

    init setup

---
+++ algo-course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algo_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Output...");
+        }
+    }
+}
